e3330a5d1bb96eea3042af18a020751deec33088
Michaël Filler <47884469+Cricot2@users.noreply.github.com>
2021-01-16 18:34:14 +0100

    Update py

--- radioOSC.py
+++ radioOSC.py
@@ -7,12 +7,19 @@ import socket
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+import vlc
+
+from urls import *
 
 current_dir = os.path.dirname(__file__)
 sound = os.path.join(current_dir, "soundfiles")
 os.makedirs(sound, exist_ok=True)
 shime = os.path.join(sound, "1.wav")
 os.popen(f"play {shime}")
+
+instance = vlc.Instance('--verbose 2'.split())
+player = instance.media_player_new()
+player.audio_set_volume(90)
 
 
 def get_ip():
@@ -32,34 +39,37 @@ server = osc_server.ThreadingOSCUDPServer((get_ip(), 5000), dispatcher)
 
 
 def killRadio():
-    os.system("sudo killall vlc")
-
+    player.stop()
+    player.play()
 
 def radio_station(args, station):
     if args == "/play": 
-        if station == 0:
-            killRadio()        
-            os.system("cvlc http://live.francra.org:8000/radiocanut --gain 1.5")
+        if station == 0:      
+            player.set_mrl(canut)
+            killRadio()  
         elif station == 1:
-            killRadio()
-            os.system("cvlc http://direct.francemusique.fr/live/francemusique-midfi.mp3 --gain 0.3")
+            player.set_mrl(musique)
+            killRadio()  
         elif station == 2:
-            killRadio()
-            os.system("cvlc http://direct.franceinfo.fr/live/franceinfo-midfi.mp3 --gain 0.3")
+            player.set_mrl(info)
+            killRadio()  
         elif station == 3:
-            killRadio()
-            os.system("cvlc http://direct.franceinter.fr/live/franceinter-midfi.mp3 --gain 0.4")
+            player.set_mrl(inter)
+            killRadio()  
         elif station == 4:
-            killRadio()
-            os.system("cvlc http://direct.radiofrance.fr/live/franceculture-midfi.mp3 --gain 0.4")
+            player.set_mrl(culture)
+            killRadio()  
         elif station == 5:
-            killRadio()
-            os.system("cvlc http://direct.fipradio.fr/live/fip-midfi.mp3 --gain 0.3")
+            player.set_mrl(fip)
+            killRadio()  
 
+def volum_handler(v, args, val):
+    print(f'{args[0]} ~ {int(val)}')
+    player.audio_set_volume(int(val))
 
 def radio_stop(args, state):
     if state == 1:
-        os.popen("sudo killall vlc")
+        player.stop()
 
 
 def shutdown(args, state):
@@ -71,4 +81,5 @@ if __name__ == "__main__":
     dispatcher.map("/play", radio_station)
     dispatcher.map("/stop", radio_stop)
     dispatcher.map("/off", shutdown)
+    dispatcher.map("/vol", volum_handler, "volume")
     server.serve_forever()
