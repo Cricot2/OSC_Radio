02921b5d12e360135a2f612766331173964f777d
Michaël Filler <47884469+Cricot2@users.noreply.github.com>
2021-01-19 15:03:49 +0100

    Update radioOSC => json storage

--- radioOSC.py
+++ radioOSC.py
@@ -4,6 +4,7 @@ import os
 import socket
 import time
 import random
+import json
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
@@ -15,9 +16,9 @@ from urls import *
 current_dir = os.path.dirname(__file__)
 sound = os.path.join(current_dir, "soundfiles")
 scrub = os.path.join(current_dir, "scrub")
-instance = vlc.Instance('--verbose 2'.split())
+instance = vlc.Instance("--verbose 2".split())
 player = instance.media_player_new()
-vol_path = os.path.join(current_dir, "vol.txt")
+storage_path = os.path.join(current_dir, "vol.json")
 
 
 def init():
@@ -26,27 +27,27 @@ def init():
     player.audio_set_volume(get_current_vol())
 
 
-def get_current_vol():
-    with open(vol_path, "r") as f:
-        vol = f.read()
-        if not vol:
-            vol = 50
-
-        return int(vol)
-
-
 def save_current_vol(val):
-    with open(vol_path, "w") as f:
-        f.write(str(val))
+    data = {"vol": val}
+    with open(storage_path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def get_current_vol():
+    with open(storage_path, "r") as f:
+        last_vol = json.load(f)
+        if not last_vol:
+            return 50
+        return int(last_vol.get("vol"))
 
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(('10.255.255.255', 1))
+        s.connect(("10.255.255.255", 1))
         IP = s.getsockname()[0]
     except Exception:
-        IP = '127.0.0.1'
+        IP = "127.0.0.1"
     finally:
         s.close()
     return IP
@@ -62,22 +63,22 @@ def play_radio(radio):
 
 
 def radio_station(args, station):
-    if args == "/play": 
-        if station == 0:      
-            play_radio(canut)  
+    if args == "/play":
+        if station == 0:
+            play_radio(canut)
         elif station == 1:
-            play_radio(musique)  
+            play_radio(musique)
         elif station == 2:
-            play_radio(info)  
+            play_radio(info)
         elif station == 3:
-            play_radio(inter)  
+            play_radio(inter)
         elif station == 4:
-            play_radio(culture)  
+            play_radio(culture)
         elif station == 5:
-            play_radio(fip)  
+            play_radio(fip)
 
 
-def volum_handler(v, args, val):
+def volume_handler(v, args, val):
     player.audio_set_volume(int(val))
     save_current_vol(int(val))
 
@@ -101,7 +102,7 @@ if __name__ == "__main__":
         dispatcher.map("/play", radio_station)
         dispatcher.map("/stop", radio_stop)
         dispatcher.map("/off", shutdown)
-        dispatcher.map("/vol", volum_handler, "volume")
+        dispatcher.map("/vol", volume_handler, "volume")
         server.serve_forever()
     except KeyboardInterrupt:
         print("STOP")
