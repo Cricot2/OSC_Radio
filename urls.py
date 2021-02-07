e535836eba1e0284393933d48f8be9d070fc119a
Michaël Filler <47884469+Cricot2@users.noreply.github.com>
2021-02-07 06:52:04 +0100

    Add vol.json and urls.py

---
+++ urls.py
@@ -0,0 +1,8 @@
+STATIONS = {
+    "canut": "http://live.francra.org:8000/radiocanut",
+    "info" : "http://direct.franceinfo.fr/live/franceinfo-midfi.mp3",
+    "musique": "http://direct.francemusique.fr/live/francemusique-midfi.mp3",
+    "inter": "http://direct.franceinter.fr/live/franceinter-midfi.mp3",
+    "culture": "http://direct.radiofrance.fr/live/franceculture-midfi.mp3",
+    "fip": "http://direct.fipradio.fr/live/fip-midfi.mp3"
+}
